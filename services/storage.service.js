c886945028561c86faefda95f4cc4babc4a0e68e
Andrey Litvinov <drune.email@gmail.com>
2026-09-21 19:48:21 +0300

    fix node:path usage and npm start

--- services/storage.service.js
+++ services/storage.service.js
@@ -1,6 +1,6 @@
-import { homedir } from 'os';
-import { join } from 'path';
-import { promises } from 'fs';
+import { homedir } from 'node:os';
+import { join } from 'node:path';
+import { readFile, writeFile, stat } from 'node:fs/promises';
 
 const filePath = join(homedir(), 'weather-data.json');
 
@@ -10,31 +10,31 @@ const TOKEN_DICTIONARY = {
 }
 
 const saveKeyValue = async (key, value) => {
-	let data = {};
-	if (await isExist(filePath)) {
-		const file = await promises.readFile(filePath);
-		data = JSON.parse(file);
-	}
-	data[key] = value;
-	await promises.writeFile(filePath, JSON.stringify(data));
+  let data = {};
+  if (await isExist(filePath)) {
+    const file = await readFile(filePath);
+    data = JSON.parse(file);
+  }
+  data[key] = value;
+  await writeFile(filePath, JSON.stringify(data));
 };
 
 const getKeyValue = async (key) => {
-	if (await isExist(filePath)) {
-		const file = await promises.readFile(filePath);
-		const data = JSON.parse(file);
-		return data[key];
-	}
-	return undefined;
+  if (await isExist(filePath)) {
+    const file = await readFile(filePath);
+    const data = JSON.parse(file);
+    return data[key];
+  }
+  return undefined;
 };
 
 const isExist = async (path) => {
-	try {
-		await promises.stat(path);
-		return true;
-	} catch (e) {
-		return false;
-	}
+  try {
+    await stat(path);
+    return true;
+  } catch {
+    return false;
+  }
 };
 
 export { saveKeyValue, getKeyValue, TOKEN_DICTIONARY };
