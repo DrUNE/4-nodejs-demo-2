825f47bb6cd8fafeb732de1c2503891824c80d0c
Andrey Litvinov <drune.email@gmail.com>
2026-09-22 16:06:43 +0300

    add save city

--- services/api.service.js
+++ services/api.service.js
@@ -1,20 +1,24 @@
-import axios from 'axios';
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
 
 const getWeather = async (city) => {
-	const token = await getKeyValue(TOKEN_DICTIONARY.token);
-	if (!token) {
-		throw new Error('Не задан ключ API, задайте его через команду -t [API_KEY]');
-	}
-	const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
-		params: {
-			q: city,
-			appid: token,
-			lang: 'ru',
-			units: 'metric'
-		}
-	});
-	return data;
+  const token = await getKeyValue(TOKEN_DICTIONARY.token);
+  if (!token) {
+    throw new Error('Не задан ключ API, задайте его через команду -t [API_KEY]');
+  }
+  const weatherApiUrl = 'http://api.weatherapi.com/v1/current.json';
+  const params = new URLSearchParams({
+    q: city,
+    key: token,
+    lang: 'ru'
+  });
+  const url = `${weatherApiUrl}?${params}`;
+  console.log(url);
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
+  }
+  const data = await response.json();
+  return data;
 };
 
 export { getWeather };
--- weather.js
+++ weather.js
@@ -5,30 +5,45 @@ import { printHelp, printSuccess, printError } from './services/log.service.js';
 import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
-	if (!token.length) {
-		printError('Не передан token');
-		return;
-	}
-	try {
-		await saveKeyValue(TOKEN_DICTIONARY.token, token);
-		printSuccess('Токен сохранён');
-	} catch (e) {
-		printError(e.message);
-	}
+  if (!token.length) {
+    printError('Не передан token');
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.token, token);
+    printSuccess('Токен сохранён');
+  } catch (e) {
+    printError(e.message);
+  }
 }
 
-const initCLI = () => {
-	const args = getArgs(process.argv)
-	if (args.h) {
-		printHelp();
-	}
-	if (args.s) {
-	}
-	if (args.t) {
-		return saveToken(args.t);
-	}
-	getWeather('moscow');
-	// Вывести погоду
+const saveCity = async (city) => {
+  if (!city.length) {
+    printError('Не передан city');
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.city, city);
+    printSuccess('City сохранён');
+  } catch (e) {
+    printError(e.message);
+  }
+}
+
+const initCLI = async () => {
+  const args = getArgs(process.argv)
+  if (args.h) {
+    printHelp();
+  }
+  if (args.s) {
+    return saveCity(args.s);
+  }
+  if (args.t) {
+    return saveToken(args.t);
+  }
+  const data = await getWeather(args.s || 'Moscow');
+  console.log(data);
+  // Вывести погоду
 };
 
-initCLI();
+await initCLI();
